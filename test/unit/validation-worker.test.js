61ae8e1b3fd1fb441ddcf0c8450b31d278879442
Jibin <jibin.mathews@getpostman.com>
2020-01-14 15:04:03 +0530

    Adding lockAcquire retries and tests

--- test/unit/validation-worker.test.js
+++ test/unit/validation-worker.test.js
@@ -4,6 +4,7 @@ var Converter = require('../..'),
   fs = require('fs'),
   path = require('path'),
   expect = require('chai').expect,
+  async = require('async'),
   VALID_OPENAPI_PATH = '../data/validationData/spec-to-validate-against.json',
   HISTORY_PATH = '../data/validationData/history_obj.json';
 
@@ -11,13 +12,12 @@ var openapi = JSON.parse(fs.readFileSync(path.join(__dirname, VALID_OPENAPI_PATH
   historyRequest = JSON.parse(fs.readFileSync(path.join(__dirname, HISTORY_PATH), 'utf8'));
 
 
-describe('Validaiton in workers', function () {
+describe.only('Validaiton in workers', function () {
   it('should validate properly', function (done) {
     const schemaPack = new Converter.SchemaPackOrchestrator({ type: 'json', data: openapi },
       { max_workers: 2, timeout: 5 * 1000 });
 
     schemaPack.validateTransaction(historyRequest, (err, result) => {
-      // result is as described in the Schema <> Validation Doc
       expect(err).to.not.exist;
 
       expect(result).to.be.ok;
@@ -32,7 +32,6 @@ describe('Validaiton in workers', function () {
       { max_workers: -1, timeout: 1 * 1000 });
 
     schemaPack.validateTransaction(historyRequest, (err, result) => {
-      // result is as described in the Schema <> Validation Doc
       expect(err).to.be.an('Error');
       expect(err.message).to.eql('MaxConcurrencyLockTimeoutError');
 
@@ -41,17 +40,78 @@ describe('Validaiton in workers', function () {
     });
   });
 
-  it('should return -ValidationTimeoutError if validation does not finish in timeout', function (done) {
+  it('should return MaxConcurrencyLockTimeoutError even if multiple instances are created', function (done) {
+    const schemaPack1 = new Converter.SchemaPackOrchestrator({ type: 'json', data: openapi },
+      { max_workers: 1, timeout: 1 * 1000, lockRetries: 0 });
+    const schemaPack2 = new Converter.SchemaPackOrchestrator({ type: 'json', data: openapi },
+      { max_workers: 1, timeout: 1 * 1000, lockRetries: 0 });
+
+    async.parallel([
+      (next) => {
+        schemaPack1.validateTransaction(historyRequest, (err, result) => {
+          return next(null, err);
+        });
+      },
+
+      (next) => {
+        schemaPack2.validateTransaction(historyRequest, (err, result) => {
+          return next(null, err);
+        });
+      }
+    ], (err, results) => {
+      if (err) {
+        return done(err);
+      }
+
+      console.log(results);
+
+      expect(results.find((result) => {
+        return !!result;
+      }).message).to.eql('MaxConcurrencyLockTimeoutError');
+
+      return done();
+    });
+  });
+
+  it('should return ValidationTimeoutError if validation does not finish in timeout', function (done) {
     const schemaPack = new Converter.SchemaPackOrchestrator({ type: 'json', data: openapi },
-      { max_workers: 2, timeout: 0 * 1000 });
+      { max_workers: 2, timeout: 10 });
 
     schemaPack.validateTransaction(historyRequest, (err, result) => {
-      // result is as described in the Schema <> Validation Doc
       expect(err).to.be.an('Error');
       expect(err.message).to.eql('ValidationTimeoutError');
 
       expect(result).to.not.exist;
       return done();
+    });
+  });
+
+  it('should allow executing parallel forks', function (done) {
+    const schemaPack = new Converter.SchemaPackOrchestrator({ type: 'json', data: openapi },
+      { max_workers: 2, timeout: 1 * 1000 });
+
+    async.parallel([
+      (next) => {
+        schemaPack.validateTransaction(historyRequest, (err, result) => {
+          expect(err).to.not.exist;
+
+          expect(result).to.be.ok;
+          expect(Object.keys(result.requests)).to.have.members(['r1', 'r2']);
+          return next();
+        });
+      },
+
+      (next) => {
+        schemaPack.validateTransaction(historyRequest, (err, result) => {
+          expect(err).to.not.exist;
+
+          expect(result).to.be.ok;
+          expect(Object.keys(result.requests)).to.have.members(['r1', 'r2']);
+          return next();
+        });
+      }
+    ], (err) => {
+      return done(err);
     });
   });
 });
